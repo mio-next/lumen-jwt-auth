e498b17e1f9d906582b524eba86f9d6eb0b0076c
Jake Oehler Morrison <jmorrison@psesd.org>
2016-02-15 17:32:19 -0800

    initial

---
+++ src/ServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @copyright Copyright (c) 2016 Canis.io
+ * @license   MIT
+ */
+
+namespace Canis\Lumen\Jwt;
+
+use Illuminate\Support\ServiceProvider;
+
+class LumenServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
